1d5022dd7daedd33485d338bdcf726c7a967263d
Franz Ibañez <jfibanez014@gmail.com>
2019-06-30 17:55:26 +0800

    Trying out iOS Swift Scroll View Paging

--- PageThisScroll/ViewController.swift
+++ PageThisScroll/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    var contentWidth: CGFloat = 0.0
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addGestureRecognizer(scrollView.panGestureRecognizer)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        let scrollViewWidth = scrollView.frame.size.width
+        let scrollViewHeight = scrollView.frame.size.height
+        let imageWidth: CGFloat = 150
+        let imageHeight: CGFloat = 150
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            var newY: CGFloat = 0.0
+            
+            newX = ((scrollViewWidth / 2) + (scrollViewWidth * CGFloat(x))) - (imageWidth / 2)
+            newY = (scrollViewHeight / 2) - (imageHeight / 2)
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX, y: newY, width: imageWidth, height: imageHeight)
+        }
+        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollViewHeight)
+    }
 
 }
 
